696d8ed0de787b1a87f7ea3569d0e68d0abd0f76
Kostya Vasilyev <kostiavasilev10@gamil.com>
2022-12-18 13:07:37 +0300

    Fix

--- ElementPosition/Program.cs
+++ ElementPosition/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите номер строки");
+Console.WriteLine("Введите номер строки: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите номер столбца");
+Console.WriteLine("Введите номер столбца: ");
 int m = Convert.ToInt32(Console.ReadLine());
 int [,] numbers = new int [10,10];
 FillArrayRandomNumbers(numbers);
